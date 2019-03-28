956c7940b58e2ee37c506d407e6faf87a04b807a
ashish2097 <ashishduklann@gmail.com>
2019-03-28 23:11:26 +0530

    Final of Lynda ... Begin of Mine

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+var appdata = require('../data.json');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var myArtwork = [];
+  var myArtists = [];
+  myArtists = appdata.speakers;
+  appdata.speakers.forEach(function(item) {
+  	myArtwork = myArtwork.concat(item.artwork);
+  });
+  res.render('index', {
+   title: 'Home',
+   artwork: myArtwork,
+   artists: myArtists,
+   page: 'home'
+   });
+});
+
+router.get('/speakers', (req, res, next)=> {
+	var myArtwork = [];
+	var myArtists = [];
+	myArtists = appdata.speakers;
+	appdata.speakers.forEach((item)=> {
+		myArtwork = myArtwork.concat(item.artwork);
+	});
+	res.render('speakers', {
+		title: 'Speakers',
+		artwork: myArtwork,
+		artists: myArtists,
+		page: 'artistList'
+	});
+});
+
+
+router.get('/speakers/:speakerid', (req, res, next)=> {
+	var myArtwork = [];
+	var myArtists = [];
+
+	appdata.speakers.forEach((item)=> {
+		if(item.shortname == req.params.speakerid) {
+			myArtists.push(item);
+			myArtwork = myArtwork.concat(item.artwork);
+		}
+	});
+	res.render('speakers', {
+		title: 'Speakers',
+		artwork: myArtwork,
+		artists: myArtists,
+		page: 'artistDetail'
+	});
+});
+
+module.exports = router;
